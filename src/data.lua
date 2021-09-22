14d0ba7d2fc13c26566df384452447589ec4e8df
ceresward <ceresward@gmail.com>
2021-09-21 22:50:29 -0400

    Updated mod to 1.1, data.lua updates
    - Updated info.json to target Factorio 1.1
    - Reviewed and improved the selection-tool and shortcut configurations based on experience with channel-changer in Logistic Network Channels

--- src/data.lua
+++ src/data.lua
@@ -1,26 +1,62 @@
--- Note: retyping to 'selection-tool' is the easiest way to prevent the item from actually marking ghosts for
--- deconstruction.  Unfortunately that also means losing the filter functionality of the deconstruction planner.  Only
--- way around this would be to use the 'deconstruction-item' type and somehow find a way to capture the deconstructed
--- ghosts and restore them same-tick as approved entities.  Even then, it may not be possible to customize entity
--- selection properly when the item is 'deconstruction-item' type...for the mod to work, it needs to be able to select
--- entities on a separate force, and in Friend Blueprints that wasn't possible with the built-in blueprint type...
-local constructionPlanner = table.deepcopy(data.raw["deconstruction-item"]["deconstruction-planner"])
-constructionPlanner.type = "selection-tool"
-constructionPlanner.icon = "__ConstructionPlanner__/graphics/icons/construction-planner.png"
-constructionPlanner.name = "construction-planner"
-constructionPlanner.selection_color = {1, 0.5, 0}
-constructionPlanner.alt_selection_color = {0.5, 0.25, 0}
-constructionPlanner.selection_cursor_box_type = "copy"
-constructionPlanner.alt_selection_cursor_box_type = "not-allowed"
+-- Note: the original hope was to use a deconstruction planner as the prototype, so I could benefit from the built-in
+-- filtering functionality.  However, there is no pragmatic way to prevent the deconstruction behavior from actually
+-- occurring, i.e. prevent the selection from actually removing the ghosts as a deconstruction planner normally does.
+-- So instead, I'm forced to use a selection-tool, and accept the fact that the entity highlighting won't work unless
+-- Wube adds support for it in some future update.
+local constructionPlanner = {
+    type = "selection-tool",
+    name = "construction-planner",
+    order = "c[automated-construction]-b[construction-planner]",
+    icons = {
+        {icon = "__ConstructionPlanner__/graphics/icons/construction-planner.png", icon_size = 64, icon_mipmaps = 4},
+        {icon = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x32-white.png", icon_size = 32, scale = 0.75},
+    },
+    flags = {"hidden", "not-stackable", "spawnable", "only-in-cursor"},
+    subgroup = "other",
+    stack_size = 1,
+    selection_color = {1, 0.5, 0},
+    alt_selection_color = {1, 0.5, 0},
+    selection_mode = {"nothing"},
+    alt_selection_mode = {"nothing"},
+    selection_cursor_box_type = "copy",
+    alt_selection_cursor_box_type = "copy",
+}
 
-local giveConstructionPlanner = table.deepcopy(data.raw["shortcut"]["give-deconstruction-planner"])
-giveConstructionPlanner.name = "give-construction-planner"
-giveConstructionPlanner.localised_name = nil
-giveConstructionPlanner.icon.filename = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x32-white.png";
-giveConstructionPlanner.small_icon.filename = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x24.png";
-giveConstructionPlanner.disabled_small_icon.filename = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x24-white.png";
-giveConstructionPlanner.item_to_create = "construction-planner"
-giveConstructionPlanner.style = "green",
+local giveConstructionPlanner = {
+    type = "shortcut",
+    name = "give-construction-planner",
+    order = "b[blueprints]-g[construction-planner]",
+    action = "spawn-item",
+    localised_name = nil,
+    associated_control_input = "give-construction-planner",
+    technology_to_unlock = "construction-robotics",
+    item_to_spawn = "construction-planner",
+    style = "green",
+    icon = {
+        filename = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x32-white.png",
+        priority = "extra-high-no-scale",
+        size = 32,
+        scale = 0.5,
+        mipmap_count = 2,
+        flags = {"gui-icon"}
+    },
+    small_icon = {
+        filename = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x24-white.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 0.5,
+        mipmap_count = 2,
+        flags = {"gui-icon"}
+    },
+    disabled_small_icon = {
+        filename = "__ConstructionPlanner__/graphics/icons/shortcut-toolbar/mip/new-construction-planner-x24-white.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 0.5,
+        mipmap_count = 2,
+        flags = {"gui-icon"}
+    }
+}
 
 data:extend{
     constructionPlanner,
