cac4d4ed74d167931f4cfeaf5fd16982118b8540
ceresward <ceresward@gmail.com>
2021-09-25 00:15:04 -0400

    Finished implementing basic mod functionality
    - Approval badges are now implemented
    - Alt-selection on the approval tool now unapproves previously-approved entities
    - Newly created ghosts (via on_player_built) are now auto-unapproved
    - Created a remoted interface for manually scanning and updating entity badges (used for debugging/testing only)
    - Started preparing for 1.0 release

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,29 @@
+-- TODO: test tile ghost behavior
+-- TODO: update README.md, changelog, etc. in prep for 1.0 release
+-- TODO: flesh out post-1.0 roadmap.  Ideas:
+  -- Replace 'draw_text' with 'draw_sprite' and a better icon? (maybe hammer or hammer-and-wrench?)
+  -- Improved graphics for the shortcut and/or selection-tool, maybe thumbnail as well
+  -- Use "on_entity_changed_force", if Wube decides to add it
+  -- Forces library?
+  -- More efficient force-based logic?  (regex = slow)
+  -- Some sort of toggle switch to temporarily turn off the mod (i.e. move all ghosts to main force, hide badge, etc.) to make it easier to work around BP/upgrade limitations
+  -- It might be possible to somehow use faux/placeholder ghost entities, instead of entity force manipulation.  This might be friendlier to BP/upgrading (not guaranteed though)
+  -- Mod options, e.g. whether entities should be approved or unapproved when first built (default unapproved)
+-- TODO: note limitations
+  -- Blueprint, copy/paste, upgrade and deconstruction planners won't work on unapproved entities due to their nature as being on a separate force
+    -- Undo/redo does work, though
+    -- Workaround: approve then deconstruct
+-- TODO: release 1.0
+-- TODO: make mod API request in forums (not very hopeful but why not try...)
+  -- Would like a flag to mark a force as 'ally' or something like that...with the idea being that blueprints, copy/cut/paste, upgrade planner, decon planner, etc. would all work on allied force entities the same as though they were on the player's force
+  -- It would follow to share logistics too...but this would mess up my mods!  Not sure yet how to approach that.
+    -- Maybe suggest a flag name of 'share_blueprints' or something like that.  Or maybe just suggest 'friend' ought to imply selection-tool functionality (it lets you manually deconstruct already, after all)
 
+-- Note: there is no "on_entity_changed_force" event, so I'll have to just update badges as I change the forces in my
+--       mod, and hope that other mods don't mess around with the forces too much.  For /editor force changes during
+--       testing, I can use a console command + remote interface to manually force a badge rescan.
+
+local approvalBadges = require("control.approvalBadges")
 
 FORCE_REGEX = "(.+)%.unapproved_ghosts"
 function is_unapproved_ghost_force_name(force_name)
@@ -43,8 +68,6 @@ function get_or_create_unapproved_ghost_force(base_force)
     base_force.set_friend(unapproved_ghost_force, true)
     base_force.set_cease_fire(unapproved_ghost_force, true)
     syncAllDiplomacy(base_force, unapproved_ghost_force)
-    -- TODO: is tech sync necessary?  (I'm thinking not, b/c it's just ghosts...but could be wrong)
-    -- syncAllTechToChannel(base_force, channel)
     FORCE_CREATION_IN_PROGRESS = false
   end
   return game.forces[unapproved_ghost_force_name]
@@ -62,6 +85,27 @@ function approve_entities(entities)
     end
     if (entity.force ~= base_force) then
       entity.force = base_force
+      local badgeId = approvalBadges.getOrCreate(entity);
+      approvalBadges.showApproved(badgeId)
+    end
+  end
+end
+
+function unapprove_entities(entities)
+  local unapprovedForceCache = {}
+
+  for _, entity in pairs(entities) do
+    if not is_unapproved_ghost_force_name(entity.force.name) then
+      local unapproved_force = unapprovedForceCache[entity.force.name]
+      if not unapproved_force then
+        unapproved_force = get_or_create_unapproved_ghost_force(entity.force)
+        unapprovedForceCache[entity.force.name] = unapproved_force
+      end
+      if (entity.force ~= unapproved_force) then
+        entity.force = unapproved_force
+        local badgeId = approvalBadges.getOrCreate(entity);
+        approvalBadges.showUnapproved(badgeId)
+      end
     end
   end
 end
@@ -72,54 +116,105 @@ end
 -------------------------------------------------------------------------------
 
 script.on_event(defines.events.on_force_friends_changed,
-    function(event)
-      if not DIPLOMACY_SYNC_IN_PROGRESS and not FORCE_CREATION_IN_PROGRESS then 
-        local unapproved_ghost_force = game.forces[to_unapproved_ghost_force_name(event.force.name)]
-        if unapproved_ghost_force ~= nil then
-          -- game.print("Syncing friends update from " .. event.force.name .. " to " .. unapproved_ghost_force.name)
-          -- game.print("  (other force = " .. event.other_force.name .. ", added = " .. tostring(event.added) .. ")")
-          unapproved_ghost_force.set_friend(event.other_force, event.added)
-        end
+  function(event)
+    if not DIPLOMACY_SYNC_IN_PROGRESS and not FORCE_CREATION_IN_PROGRESS then 
+      local unapproved_ghost_force = game.forces[to_unapproved_ghost_force_name(event.force.name)]
+      if unapproved_ghost_force ~= nil then
+        -- game.print("Syncing friends update from " .. event.force.name .. " to " .. unapproved_ghost_force.name)
+        -- game.print("  (other force = " .. event.other_force.name .. ", added = " .. tostring(event.added) .. ")")
+        unapproved_ghost_force.set_friend(event.other_force, event.added)
       end
     end
+  end
 )
 
 script.on_event(defines.events.on_force_cease_fire_changed,
-    function(event)
-      if not DIPLOMACY_SYNC_IN_PROGRESS and not FORCE_CREATION_IN_PROGRESS then 
-        local unapproved_ghost_force = game.forces[to_unapproved_ghost_force_name(event.force.name)]
-        if unapproved_ghost_force ~= nil then
-          -- game.print("Syncing cease-fire update from " .. event.force.name .. " to " .. unapproved_ghost_force.name)
-          -- game.print("  (other force = " .. event.other_force.name .. ", added = " .. tostring(event.added) .. ")")
-          unapproved_ghost_force.set_cease_fire(event.other_force, event.added)
-        end
+  function(event)
+    if not DIPLOMACY_SYNC_IN_PROGRESS and not FORCE_CREATION_IN_PROGRESS then 
+      local unapproved_ghost_force = game.forces[to_unapproved_ghost_force_name(event.force.name)]
+      if unapproved_ghost_force ~= nil then
+        -- game.print("Syncing cease-fire update from " .. event.force.name .. " to " .. unapproved_ghost_force.name)
+        -- game.print("  (other force = " .. event.other_force.name .. ", added = " .. tostring(event.added) .. ")")
+        unapproved_ghost_force.set_cease_fire(event.other_force, event.added)
       end
     end
+  end
 )
 
 script.on_event(defines.events.on_player_selected_area,
-    function(event)
-        -- game.print("test")
+  function(event)
+    if event.item == 'construction-planner' then
+      local player = game.get_player(event.player_index)
 
-        if event.item == 'construction-planner' then
-            local player = game.get_player(event.player_index)
+      -- Filter should only match 'unapproved' ghosts (ghost entities on the selecting player's unapproved ghost force)
+      local entities = event.surface.find_entities_filtered {
+        area = event.area,
+        force = get_or_create_unapproved_ghost_force(player.force),
+        type = "entity-ghost"
+      }
 
-            -- TODO: fix the filtering to match only ghosts on the 'non-approved' list for the player (and maybe also the player's friends/allies?)
-            local entities = event.surface.find_entities_filtered {
-                area = event.area,
-                force = get_or_create_unapproved_ghost_force(player.force)
-            }
+      if #entities > 0 then
+        -- game.print("construction-planner: approving "..tostring(#entities).." entities")
 
-            if #entities > 0 then
-                -- TODO: comment this out once everything is working
-                game.print("construction-planner: approving "..tostring(#entities).." entities")
+        approve_entities(entities)
 
-                -- TODO: implement logic to actually change the entity force and mark it as approved
-                approve_entities(entities)
+        -- Note:  if the devs ever add support, I can also use "utility/upgrade_selection_started" at selection start
+        player.play_sound { path = "utility/upgrade_selection_ended" }
+      end
+    end
+  end
+)
 
-                -- Note:  if the devs ever add support, I can also use "utility/upgrade_selection_started" at selection start
-                player.play_sound { path = "utility/upgrade_selection_ended" }
-            end
+script.on_event(defines.events.on_player_alt_selected_area,
+  function(event)
+    if event.item == 'construction-planner' then
+        local player = game.get_player(event.player_index)
+
+        -- Filter should only match 'approved' ghosts (ghost entities on the selecting player's base force)
+        local entities = event.surface.find_entities_filtered {
+          area = event.area,
+          force = player.force,
+          type = "entity-ghost"
+        }
+
+        if #entities > 0 then
+          -- game.print("construction-planner: unapproving "..tostring(#entities).." entities")
+
+          unapprove_entities(entities)
+
+          -- Note:  if the devs ever add support, I can also use "utility/upgrade_selection_started" at selection start
+          player.play_sound { path = "utility/upgrade_selection_ended" }
         end
     end
+  end
 )
+
+script.on_event(defines.events.on_built_entity,
+  function(event)
+    -- game.print("construction-planner: detected new ghost entity (".. event.created_entity.ghost_name ..")")
+    unapprove_entities({event.created_entity})
+  end,
+  {{ filter="type", type="entity-ghost"}}
+)
+
+-------------------------------------------------------------------------------
+--       REMOTE INTERFACES (comment out when not debugging)
+-------------------------------------------------------------------------------
+
+-- -- /c remote.call("constructionplanner","badgeScan")
+-- remote.add_interface("constructionplanner", {
+--   badgeScan = function()
+--     ghostEntities = game.player.surface.find_entities_filtered {
+--       type = "entity-ghost"
+--     }
+--     game.print("construction-planner: scanning badges for  "..tostring(#ghostEntities).." ghost entities")
+--     for _, entity in pairs(ghostEntities) do
+--       local badgeId = approvalBadges.getOrCreate(entity);
+--       if is_unapproved_ghost_force_name(entity.force.name) then
+--         approvalBadges.showUnapproved(badgeId)
+--       else
+--         approvalBadges.showApproved(badgeId)
+--       end
+--     end
+--   end
+-- })
---
+++ src/control/approvalBadges.lua
@@ -0,0 +1,66 @@
+local approvalBadges = {}
+
+local badgeScale = 2
+
+-- Unicode marks, for convenience:
+--   Useable:  ✘ ✔
+--   Not useable:  🛠
+
+-----------------------------------------------------------
+--  Public functions
+-----------------------------------------------------------
+
+function approvalBadges.getOrCreate(entity)
+  if not global.approvalBadges then
+    global.approvalBadges = {}
+  end
+  if not global.approvalBadges[entity.unit_number] then
+    global.approvalBadges[entity.unit_number] = rendering.draw_text {
+      text = "",
+      -- text = "██",  -- Can be used for checking text bounding box / alignment
+      surface = entity.surface,
+      target = entity,
+      -- 5/16 ratio is techically closer to center, but it kinda looks better at 1/4
+      --target_offset = {0, -badgeScale*5/16},
+      target_offset = {0, -badgeScale/4},
+      color = {0.5, 0.5, 0.5},
+      -- players = {playerIndex},
+      alignment = "center",
+      scale = badgeScale,
+    }
+  end
+  return global.approvalBadges[entity.unit_number]
+end
+
+function approvalBadges.showApproved(badgeId)
+  if badgeId and rendering.is_valid(badgeId) then
+    rendering.set_text(badgeId, "✔")
+    rendering.set_color(badgeId, {0.0, 0.8, 0.0, 0.6})
+    rendering.set_visible(badgeId, true)
+  end
+end
+
+function approvalBadges.showUnapproved(badgeId)
+  if badgeId and rendering.is_valid(badgeId) then
+    rendering.set_text(badgeId, "✔")
+    rendering.set_color(badgeId, {0.5, 0.5, 0.5, 0.4})
+    rendering.set_visible(badgeId, true)
+  end
+end
+
+function approvalBadges.hide(badgeId)
+  if badgeId and rendering.is_valid(badgeId) then
+    rendering.set_text(badgeId, "")
+    rendering.set_visible(badgeId, false)
+  end
+end
+
+-----------------------------------------------------------
+--  Private functions
+-----------------------------------------------------------
+
+
+
+-----------------------------------------------------------
+
+return approvalBadges
